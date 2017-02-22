94ab53b9ea4f3ab93a482d5a9049b7664ccebce1
demonsdemon <paul.allred@gmail.com>
2017-02-22 12:42:26 -0700

    Merge remote-tracking branch 'refs/remotes/origin/master' into villagesetup
    # Conflicts:
    #	Assets/Scripts/StructsAndEnums.cs

---
+++ Assets/Scripts/Asset Scripts/BuildingAsset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Building", menuName = "SuperStreamers/DataObjects", order = 2)]
+public class BuildingAsset : ScriptableObject {
+
+	public string buildingName;
+	public Sprite icon;
+	public GameObject modelPrefab;
+	public BuildingType buildingType;
+	public int buildingLevel = 1;
+
+	public BuildingRequirement[] requirements;
+
+	public float cost;
+
+	public Resource generatedResource;
+	public float resourcesPerSecond;
+
+	public Resource consumedResource1;
+	public Resource consumedResource2;
+	public Resource producedResource;
+	public float resource1ConsumptionRate;
+	public float resource2ConsumptionRate;
+	public float productionRate;
+}
