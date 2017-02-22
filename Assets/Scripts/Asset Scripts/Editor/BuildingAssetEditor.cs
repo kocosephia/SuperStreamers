374f76758a86c8b99e22f097b1312d38703c5ddc
Micah West <micahdavidwest@gmail.com>
2017-02-21 21:20:49 -0700

    started on buildings

---
+++ Assets/Scripts/Asset Scripts/Editor/BuildingAssetEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BuildingAsset))]
+public class BuildingAssetEditor : Editor {
+
+	SerializedProperty lookAtPoint;
+
+	void OnEnable()
+	{
+		lookAtPoint = serializedObject.FindProperty("lookAtPoint");
+	}
+
+		public override void OnInspectorGUI()
+	{
+		serializedObject.Update ();
+		EditorGUILayout.PropertyField (lookAtPoint);
+		serializedObject.ApplyModifiedProperties ();
+		if (lookAtPoint.vector3Value.y > (target as LookAtPoint).transform.position.y) {
+			EditorGUILayout.LabelField ("(Above this object)");
+		}
+		if (lookAtPoint.vector3Value.y < (target as LookAtPoint).transform.position.y) {
+			EditorGUILayout.LabelField ("(Below this object)");
+		}
+	}
+
+}
--- Assets/Scripts/StructsAndEnums.cs
+++ Assets/Scripts/StructsAndEnums.cs
@@ -1,3 +1,13 @@
 ﻿
 
 public enum Permanency { STREAM, ASCENTION, PERMANENT};
+
+public enum BuildingType {HOUSE, PRODUCER1, PRODUCER2, CONVERTER1, CONVERTER2, TECH_BUILDING1, TECH_BUILDING2} 
+
+public enum Resource {DIRT, MUD, STONE, TIN, IRON, GOLD, URANIUM}
+
+public struct BuildingRequirement
+{
+	public BuildingType buildingType;
+	public int buildingLevel;
+}
